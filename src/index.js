7662d50ac167b4dbef69dde898d0e13e2ffee63a
veranika <veronica.stanevich@gmail.com>
2019-10-13 00:43:54 +0300

    feat zeros implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function zeros(expression) {
-  // your solution
+function factorial_1(n) {
+    if (n === 0n || n === 1n) {
+        return 1n;
+    }
+    if (n > 0n) {
+        return n * factorial_1(n - 1n);
+    }
 }
+
+function factorial_2(n) {
+    if (n === 0n || n === 1n) {
+        return 1n;
+    }
+    if (n > 0n) {
+        return n * factorial_2(n - 2n);
+    }
+}
+
+module.exports = function zeros(expression) {
+    const factorials = expression.split("*");
+    const result = factorials.reduce((acc, str) => {
+        if (str.endsWith("!!")) {
+            const str2 = str.substr(0, str.length - 2);
+            return acc * BigInt(factorial_2(BigInt(str2)));
+        }
+
+        const str1 = str.substr(0, str.length - 1);
+        const factorialValue = factorial_1(BigInt(str1));
+        return acc * BigInt(factorialValue);
+    }, BigInt(1));
+
+    const resultString = String(result);
+    const resultArray = resultString.split("").reverse();
+    return resultArray.findIndex(value => value !== "0");
+};
